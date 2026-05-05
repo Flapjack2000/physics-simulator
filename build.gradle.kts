f3cd31acd9036e117517911ea1c42974abe8d696
Zachary Williams <zacharywilliams080802@gmail.com>
2026-05-05 17:37:26 -0400

    added LWJGL, GLFW, OpenGL, NanoVG dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,24 @@ plugins {
 group = "com.flapjackzach"
 version = "1.0-SNAPSHOT"
 
+val lwjglVersion = "3.4.1"
+val lwjglNatives = "natives-windows"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nanovg")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
